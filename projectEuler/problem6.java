d1c24248daffb8d1a29814b5fbbc42adf60d346c
mahirahmed <mahirahmed691@gmail.com>
2017-09-12 15:11:37 +0100

    Solution to problem 6.

---
+++ projectEuler/problem6.java
@@ -0,0 +1,19 @@
+package projectEuler;
+public class problem6 {
+
+    public static void main(String[] args) {
+
+        int squares = 0;
+        int sums = 0;
+        int finalSums = 0;
+        for (int i = 1; i <= 100; i++) {
+            squares += Math.pow(i, 2);
+        }
+        for (int i = 1; i <= 100; i++) {
+            sums += i;
+        }
+        finalSums += Math.pow(sums, 2);
+        System.out.println(finalSums - squares);
+    }
+}
+
