78cfd49c6cdab67ce1352f8bad630ae97ac3891f
mahirahmed <mahirahmed691@gmail.com>
2017-09-12 15:05:51 +0100

    Solution to problem 3.

---
+++ projectEuler/problem3.java
@@ -0,0 +1,23 @@
+package projectEuler;
+import java.util.ArrayList;
+
+public class problem3 {
+    // Largest prime factors
+    private static ArrayList<Long> largestPrimeFactor(long num) {
+
+        ArrayList<Long> factors = new ArrayList<>();
+        for (long i = 2; i <= num; i++) {
+            while (num % i == 0) {
+                factors.add(i);
+                num /= i;
+            }
+        }
+        return factors;
+    }
+    public static void main(String[] args) {
+
+        for (Long lon : largestPrimeFactor(600851475143L)) {
+            System.out.println(lon);
+        }
+    }
+}
