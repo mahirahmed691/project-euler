456c9599e63dfc294d57a268a1ceb8461cb14cff
mahirahmed <mahirahmed691@gmail.com>
2017-09-12 15:09:55 +0100

    Solution to problem 5.

---
+++ projectEuler/problem5.java
@@ -0,0 +1,30 @@
+package projectEuler;
+public class problem5 {
+
+
+    // Smallest multiple
+    public static void main (String[] args){
+
+        int smallest = 0;
+
+        for(int num = 2; num < 1000000000; num += 2) {
+            boolean isMultiple = true;
+
+            for (int multiple = 1; multiple <= 20; multiple++) {
+
+                if (num % multiple != 0) {
+                    isMultiple = false;
+                }
+            }
+
+            if (isMultiple) {
+                smallest = num;
+                break;
+            }
+        }
+
+        System.out.println(smallest);
+
+    }
+
+}
