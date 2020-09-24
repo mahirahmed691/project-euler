23bb306f261f3a822787348eb0dbd36314e27986
Mahir Ahmed <mahirahmed691@gmail.com>
2020-09-24 02:23:40 +0100

    java rename file

---
+++ Solutions/_001/001.java
@@ -0,0 +1,18 @@
+package Solutions._001;
+public class 001 {
+    public static void main(String[] args) {
+
+        sum();
+    }
+
+    private static void sum() {
+
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
