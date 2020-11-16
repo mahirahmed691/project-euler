5c1b148ce6d324264803ab38b3d9efc833315171
mahirahmed691 <mahirahmed691@gmail.com>
2020-11-16 14:37:01 +0000

    php sol 1

---
+++ Solutions/_001/001.php
@@ -0,0 +1,17 @@
+<?php
+
+$divisors = array(3, 5);
+$multiples_of_divisors = array();
+
+for ($i = 1, $n = 1000; $i < $n; $i++) {
+    foreach ($divisors as $divisor) {
+        if (0 == $i % $divisor) {
+            $multiples_of_divisors[] = $i;
+            break;
+        }
+    }
+}
+
+$sum_of_multiples = array_sum($multiples_of_divisors);
+
+print "The solution to Euler Project is <strong>$sum_of_multiples</strong> and the individual numbers are:<br /><br />";
